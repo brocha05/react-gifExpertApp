a1e741b111710951acb0a0175c5582bf847ffd20
Brocha05 <brocha_05@hotmail.com>
2023-03-20 10:58:25 -0600

    a

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -9,7 +9,7 @@ export const GifExpertApp = () => {
     setCategory( [value, ...category] )
   }
 
-  console.log("Hi wrl")
+  console.log("Hi wrasfml")
   return (
     <>
       <h1>GifExpertApp</h1>
